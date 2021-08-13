924d63d48b651465263e1b2d097c0cdd67a8e0f7
DaanGamesDG <dwklarenbeek@gmail.com>
2021-08-13 22:37:46 +0200

    changed to @daangamesdg/logger with extensions

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,19 @@
-import { PartialTypes } from "discord.js";
 import { config } from "dotenv";
 config();
 
 import Client from "./client/Client";
 
 const owners = process.env.OWNERS?.split(" ") ?? [];
-const partials = (process.env.PARTIALS?.split(" ") ?? []) as PartialTypes[];
 
 new Client({
 	intents: ["GUILDS", "GUILD_MESSAGES"],
+	partials: [],
 	debug: !!process.env.DEBUG,
 	activity: [
 		{
-			type: process.env.TYPE as
-				| "PLAYING"
-				| "STREAMING"
-				| "LISTENING"
-				| "WATCHING"
-				| "CUSTOM"
-				| "COMPETING",
-			name: process.env.MESSAGE,
-			url: process.env.URL,
+			type: "LISTENING",
+			name: "DaanGamesDG",
 		},
 	],
 	owners,
-	partials,
 }).start();
