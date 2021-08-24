ff3fd6b747d9f98263b09e5a9ee0bc960cf922aa
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-24 21:26:09 +0200

    Feat(BlacklistManager): Added whitelist function

--- src/client/structures/BlacklistManager.ts
+++ src/client/structures/BlacklistManager.ts
@@ -15,6 +15,12 @@ export default class BlacklistManager {
 		await this.client.prisma.botBlacklist.create({ data: { id } });
 	}
 
+	public async whitelist(id: string): Promise<void> {
+		if (!this.blacklisted.includes(id)) return;
+		this.blacklisted = this.blacklisted.filter((x) => x !== id);
+		await this.client.prisma.botBlacklist.delete({ where: { id } });
+	}
+
 	public setBlacklisted(blacklisted: string[]): this {
 		this.blacklisted = blacklisted;
 
