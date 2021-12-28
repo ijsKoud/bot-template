af3fb0f54935a460f10f629d32cdd90f10e7ef84
DaanGamesDG <daan@daangamesdg.xyz>
2021-12-28 21:54:55 +0100

    fix: compiling issues

--- src/bot/preconditions/Blacklisted.ts
+++ src/bot/preconditions/Blacklisted.ts
@@ -20,7 +20,7 @@ export default class extends Precondition {
 	}
 
 	private InteractionRun(interaction: Interaction): Precondition.Result {
-		return this.client.blacklistManager.isBlacklisted(interaction.user.id, interaction.guildId)
+		return this.client.blacklistManager.isBlacklisted(interaction.user.id, interaction.guildId ?? "")
 			? this.error({
 					message:
 						"You or this server is blacklisted, you can no longer use this bot. If you think that this is a mistake, please DM one of the developers of this bot!"
